2032eb979a8d4f5755b2c151fffa8580b315c3ce
Dinis Cruz <dinis.cruz@owasp.org>
2021-01-21 02:20:51 +0000

    added first list of live icap servers and some code to load it

--- k8_live_servers/Live_Servers.py
+++ k8_live_servers/Live_Servers.py
@@ -1,2 +1,14 @@
+from osbot_utils.utils.Files import path_combine, file_contents
+from osbot_utils.utils.Yaml import yaml_load
+
+
 class Live_Servers:
-    pass
+    def __init__(self):
+        self.path_live_servers = path_combine('../../data', 'icap-servers.yaml')
+
+    def icap_aws_ec2(self):
+        return self.icap().get('aws-ec2')
+
+    def icap(self):
+        return yaml_load(self.path_live_servers)
+
---
+++ tests/unit/test_Live_Servers.py
@@ -0,0 +1,24 @@
+from pprint import pprint
+from unittest import TestCase
+
+from osbot_utils.utils.Files import file_exists
+
+from k8_live_servers.Live_Servers import Live_Servers
+from osbot_elastic.helpers._to_add_to_osbot.OSBot_Utils__Local import sorted_set
+
+
+class test_Live_Servers(TestCase):
+
+    def setUp(self):
+        self.live_servers = Live_Servers()
+        print()
+
+    def test__init__(self):
+        assert file_exists(self.live_servers.path_live_servers)
+
+    def test_icap_aws_ec2(self):
+        assert len(self.live_servers.icap_aws_ec2()) > 0
+
+    def test_icap(self):
+        sorted_set(self.live_servers.icap()) == ['aws-ec2', 'azure-aks', 'azure-rancher', 'vmware-esxi', 'vmware-vcenter']
+        pprint(self.live_servers.icap())
