070d3d096a6a2c41b1f62c9e918022b977e851a0
Dinis Cruz <dinis.cruz@owasp.org>
2021-01-21 06:33:12 +0000

    data fix

--- k8_live_servers/lambdas/elastic_server_details.py
+++ k8_live_servers/lambdas/elastic_server_details.py
@@ -1,16 +1,12 @@
-import os
+import json
 
-from osbot_aws.Dependencies import load_dependency, load_dependencies
-from osbot_aws.apis.S3 import S3
-from osbot_utils.utils.Files import Files
+from osbot_aws.Dependencies import load_dependencies
 from osbot_utils.utils.Json import json_dumps
-
-
 
 
 def run(event, context=None):
     load_dependencies('elasticsearch,requests')
     from k8_live_servers.elastic.Elastic_Icap import Elastic_Icap
-    return Elastic_Icap().index().info()
 
-    return 'going to send to elastic the server details'
+    return json.dumps(list(Elastic_Icap().index().data()))
+
